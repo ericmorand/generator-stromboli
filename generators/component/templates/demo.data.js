70e1085edfb7a03548e3c9d20e88700e8027efdb
Eric MORAND <emorand@wideagency.com>
2017-02-26 21:26:09 +0100

    Improve demo templates

--- generators/component/templates/demo.data.js
+++ generators/component/templates/demo.data.js
@@ -1,5 +1,9 @@
 module.exports = function(plugin) {
   return {
+    demo: {
+      title: '<%= componentName %> demo',
+      class: '<%= componentCleanName %>-demo'
+    },
     content: 'Lorem ipsum'
   };
 };
--- test/component/wanted/demo.data.js
+++ test/component/wanted/demo.data.js
@@ -1,5 +1,9 @@
 module.exports = function(plugin) {
   return {
+    demo: {
+      title: 'lorem/ipsum demo',
+      class: 'lorem-ipsum-demo'
+    },
     content: 'Lorem ipsum'
   };
 };
